f6a3e7cba697a2387c2dc2c616038823f65b81b8
Rishat <virisound@gmail.com>
2022-04-29 06:31:02 +0500

    Add file test.js or test coding

---
+++ test.js
@@ -0,0 +1,6 @@
+// Урок №1: "Node.js #1 Базовые концепции и установка (Basic Concepts & Setup Environment)": https://youtu.be/NOiPCc5OUuw?list=PLNkWIWHIRwMFtsaJ4b_wwkJDHKJeuAkP0
+// Тайм-код: 6:30 ссылка: https://youtu.be/NOiPCc5OUuw?list=PLNkWIWHIRwMFtsaJ4b_wwkJDHKJeuAkP0&t=391
+
+const userName = 'Rishat';
+const sayHi = (userName) => `Hello, my name is ${userName}!`;
+console.log(sayHi(userName));
